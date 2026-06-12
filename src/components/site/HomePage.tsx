ab9523c0b34f6fa5facab67f677fdb0aca48ef07
kizito <kizitoomene120@gmail.com>
2026-06-12 09:32:22 +0100

    updated something

--- src/components/site/HomePage.tsx
+++ src/components/site/HomePage.tsx
@@ -1600,7 +1600,7 @@ function LeadershipShowcase() {
   return (
     <section
       ref={sectionRef}
-      className="relative overflow-hidden py-32 bg-[#020d1f]"
+      className="relative overflow-hidden py-32 bg-[#0e6ad3]"
       onMouseMove={(e) => {
         const rect = e.currentTarget.getBoundingClientRect();
         setMousePosition({
